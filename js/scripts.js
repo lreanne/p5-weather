a7955ab95fa43d263a5f245707d53e4c6b5b78ab
lreanne <reannelee@Reanne-Lees-MacBook-Pro.local>
2014-12-09 22:14:27 -0800

    version 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // REF: http://foundation.zurb.com/docs/
 // REF: http://simpleweatherjs.com/
 // On click button, get zip, then run Simple Weather
-$('button').on('click', function() {
+$('.button').on('click', function() {
   
   // 1. Get & store entered zipcode
   var zipcode = $('#getWeather').val();
@@ -45,11 +45,13 @@ $(document).ready(function() {
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-  
+      //html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      //html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      //html += '<li class="currently">'+weather.currently+'</li>';
+      //html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+     $(".temp").text(weather.temp);
+     $(".city").text(weather.city);
+
       $("#weather").html(html);
     },
     error: function(error) {
