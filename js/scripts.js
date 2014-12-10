e6b61ac6488d094ff2934556111c502ae867db1b
lreanne <reannelee@Reanne-Lees-MacBook-Pro.local>
2014-12-09 21:33:57 -0800

    version 1

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+// REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com/
+// On click button, get zip, then run Simple Weather
+$('button').on('click', function() {
+  
+  // 1. Get & store entered zipcode
+  var zipcode = $('#getWeather').val();
+  
+  // 2. Pass weather into _simpleWeather()_ object
+  $.simpleWeather({
+    
+    location: zipcode,
+  
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+
+      // See console for all properties of object
+      console.log(weather);
+    },
+  
+    error: function(error) {
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
+  
+  // 3. Reset input value
+  $('#getWeather').val('');
+  
+});
+
+$(document).ready(function() {
+  
+
+  $.simpleWeather({
+    location: 'Austin, TX',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
+
+
